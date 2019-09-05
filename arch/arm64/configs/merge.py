f544bdd96ad4a6e4a675c0eda0978c6ce444c70f
George Hotz <george@comma.ai>
2019-09-04 21:46:31 -0700

    tici defconfig update

---
+++ arch/arm64/configs/merge.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+a1 = set(open("tici_defconfig").read().split("\n"))
+a2 = set(open("tici_defconfig_old").read().split("\n"))
+
+for x in a1:
+  if x not in a2:
+    print(x)
+
+
