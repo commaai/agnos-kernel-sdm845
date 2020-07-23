e0f7461d12524a49b6b2c0b121831b0241bb1ad7
Robbe Derks <robbe.derks@gmail.com>
2020-07-23 19:31:23 +0200

    fix slow uart comms

--- drivers/tty/serial/msm_geni_serial.c
+++ drivers/tty/serial/msm_geni_serial.c
@@ -2543,7 +2543,7 @@ static int msm_geni_serial_probe(struct platform_device *pdev)
 								GFP_KERNEL);
 	} else {
 		pm_runtime_set_suspended(&pdev->dev);
-		pm_runtime_set_autosuspend_delay(&pdev->dev, 150);
+		pm_runtime_set_autosuspend_delay(&pdev->dev, 10000);  // Longer autosuspend delay
 		pm_runtime_use_autosuspend(&pdev->dev);
 		pm_runtime_enable(&pdev->dev);
 	}
