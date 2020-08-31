bc45c93cdb4b82ebe74185bc1fe4f12a926dc1cb
Willem Melching <willem.melching@gmail.com>
2020-08-31 16:13:05 +0200

    perf build fixes

--- tools/perf/util/pmu.c
+++ tools/perf/util/pmu.c
@@ -325,7 +325,7 @@ static int pmu_aliases_parse(char *dir, struct list_head *head)
 		if (pmu_alias_info_file(name))
 			continue;
 
-		snprintf(path, PATH_MAX, "%s/%s", dir, name);
+		assert(snprintf(path, PATH_MAX, "%s/%s", dir, name));
 
 		file = fopen(path, "r");
 		if (!file) {
