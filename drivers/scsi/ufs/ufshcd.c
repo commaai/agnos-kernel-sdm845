b0a861033b121848e74f322a69994924d0d5cc14
Robbe Derks <robbe.derks@gmail.com>
2023-03-23 13:52:24 +0100

    reduce kernel spam

--- drivers/scsi/ufs/ufshcd.c
+++ drivers/scsi/ufs/ufshcd.c
@@ -8476,7 +8476,7 @@ static int ufshcd_ioctl(struct scsi_device *dev, int cmd, void __user *buffer)
 
 	BUG_ON(!hba);
 	if (!buffer) {
-		dev_err(hba->dev, "%s: User buffer is NULL!\n", __func__);
+		dev_dbg(hba->dev, "%s: User buffer is NULL!\n", __func__);
 		return -EINVAL;
 	}
 
