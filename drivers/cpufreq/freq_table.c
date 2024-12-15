1825660b336f38267aceda31328cd88267226658
Adeeb Shihadeh <adeebshihadeh@gmail.com>
2024-12-15 15:01:48 -0800

    Reapply "CPU freq governor, don't use freq above 1689600" (#75)
    This reverts commit 8caff2d541da601edd5827703e35301b6cc99aec.

--- drivers/cpufreq/freq_table.c
+++ drivers/cpufreq/freq_table.c
@@ -43,6 +43,7 @@ int cpufreq_frequency_table_cpuinfo(struct cpufreq_policy *policy,
 
 	cpufreq_for_each_valid_entry(pos, table) {
 		freq = pos->frequency;
+    if (freq > 1689600) continue;
 
 		if (!cpufreq_boost_enabled()
 		    && (pos->flags & CPUFREQ_BOOST_FREQ))
